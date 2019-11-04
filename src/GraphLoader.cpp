95100b13435e9318420292d184f945ff6cc06d7f
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2019-11-04 18:45:44 +0100

    quick fix for support of package:// uris (just remove package://)

--- src/GraphLoader.cpp
+++ src/GraphLoader.cpp
@@ -111,9 +111,19 @@ void envire::urdf::GraphLoader::loadJoints(const ::urdf::ModelInterface& urdfMod
                 //prefix local mesh paths with the urdf path
                 if (visual->geometry->type == ::urdf::Geometry::MESH){
                     ::urdf::Mesh* mesh = dynamic_cast<::urdf::Mesh*>(visual->geometry.get());
-                    //remove all afer last "/" from modelFilename
-                    size_t found = modelFilename.find_last_of("/\\");
-                    mesh->filename = modelFilename.substr(0,found) + "/" +  mesh->filename;
+                    printf("1: %s\n,",mesh->filename.c_str());
+                    
+                    if (mesh->filename.rfind("package://", 0) == 0) {
+                        //filename starts with package://
+                        //just remove it
+                        mesh->filename.erase(0,10);
+                    }else{
+                        //remove all afer last "/" from modelFilename
+                        size_t found = modelFilename.find_last_of("/\\");
+                        mesh->filename = modelFilename.substr(0,found) + "/" +  mesh->filename;
+                        printf("2: %s\n,",mesh->filename.c_str());
+                    }
+                    
                 }
  
                 //visual_itemPtr->getData().groupId = groupId;
